7d25ac8833a0178c96315bbc2ecee326c2ce13f0
Khayot Razzakov <hrazzoqov0002@gmail.com>
2025-07-11 17:27:38 +0500

    translation jazzmin for Russian

--- main/admin.py
+++ main/admin.py
@@ -3,6 +3,8 @@ from .models import *
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('category', 'name', 'brand', 'price', 'stock', 'unit', 'created_at')
+    list_filter = ('category', 'brand', 'created_at')
+    search_fields = ('name', 'brand')
 
 admin.site.register(Category)
 admin.site.register(Product, ProductAdmin)
